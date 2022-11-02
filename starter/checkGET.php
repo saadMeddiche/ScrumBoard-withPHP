8b7bcc94c429157762090d48305f65ab02501262
Saad Meddiche <saadmeddiche2004201@gmail.com>
2022-11-02 14:11:44 +0100

    Move the tasks [Complete]

--- starter/checkGET.php
+++ starter/checkGET.php
@@ -1,6 +1,7 @@
 <?php
 require "connect.php";
 
+//Pop up the update modal
 if (!empty($_GET['id'])) {
 ?>
     <script type="text/javascript">
@@ -18,6 +19,76 @@ if (!empty($_GET['id'])) {
     </script>
 <?php
 }
+
+//Move  the task Backward
+if (!empty($_GET['moveB'])) {
+    require "connect.php";
+
+    $id = $_GET['moveB'];
+
+    $requete = "SELECT dataofthetasks.status FROM dataofthetasks WHERE id='$id' ";
+
+    $query = mysqli_query($connection, $requete);
+
+    $row = mysqli_fetch_assoc($query);
+
+    if ($row['status'] == 2) {
+
+        require "connect.php";
+
+        $requete = "UPDATE `dataofthetasks` 
+                SET `status`='1' WHERE id ='$id'";
+
+        $query = mysqli_query($connection, $requete);
+    } else if ($row['status'] == 3) {
+
+        require "connect.php";
+
+
+
+        $requete = "UPDATE `dataofthetasks` 
+                SET `status`='2' WHERE id ='$id'";
+
+        $query = mysqli_query($connection, $requete);
+    }
+    header("location:index.php");
+}
+
+//Move  the task Forward
+if (!empty($_GET['moveF'])) {
+    require "connect.php";
+
+    $id = $_GET['moveF'];
+
+    $requete = "SELECT dataofthetasks.status FROM dataofthetasks WHERE id='$id' ";
+
+    $query = mysqli_query($connection, $requete);
+
+    $row = mysqli_fetch_assoc($query);
+
+    if ($row['status'] == 1) {
+
+        require "connect.php";
+
+        $requete = "UPDATE `dataofthetasks` 
+                SET `status`='2' WHERE id ='$id'";
+
+        $query = mysqli_query($connection, $requete);
+    } else if ($row['status'] == 2) {
+
+        require "connect.php";
+
+
+
+        $requete = "UPDATE `dataofthetasks` 
+                SET `status`='3' WHERE id ='$id'";
+
+        $query = mysqli_query($connection, $requete);
+    }
+    header("location:index.php");
+}
+
+
 
 
 ?>
--- starter/index.php
+++ starter/index.php
@@ -115,7 +115,9 @@
 													
 													</div>
 													<div class="ms-5 ps-1">
-														<button type="button" name="moveF" class="btn btn-warning mb-2 " > <i> <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M5.536 21.886a1.004 1.004 0 0 0 1.033-.064l13-9a1 1 0 0 0 0-1.644l-13-9A1 1 0 0 0 5 3v18a1 1 0 0 0 .536.886z"></path></svg> </i> </button>
+													<form method="post" action="checkGET.php?moveF=' . $row['id'] . '">
+														<button type="submit" name="moveF" class="btn btn-warning mb-2 " > <i> <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M5.536 21.886a1.004 1.004 0 0 0 1.033-.064l13-9a1 1 0 0 0 0-1.644l-13-9A1 1 0 0 0 5 3v18a1 1 0 0 0 .536.886z"></path></svg> </i> </button>
+													</form>
 													</div>
 												</div>
 											</div>
@@ -196,11 +198,14 @@
 														</button>
 														<a href="index.php?id=' . $row['id'] . '"><button href="update.php?id=' . $row['id'] . '"  type="button"  class="btn btn-success mb-2" name="editBtn" id="updateBtn" >  Up </button></a>
 													</div>
-													<div class="">
-														<form method="post" action="Move.php">
+													<div class="d-flex gap-1">
+														<form method="post" action="checkGET.php?moveB=' . $row['id'] . '">
 															<button type="submit" name="moveB" class="btn btn-warning mb-2 me-1" > <i> <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="m4.431 12.822 13 9A1 1 0 0 0 19 21V3a1 1 0 0 0-1.569-.823l-13 9a1.003 1.003 0 0 0 0 1.645z"></path></svg> </i> </button>
+														</form>
+														<form method="post" action="checkGET.php?moveF=' . $row['id'] . '">
 															<button type="submit" name="moveF" class="btn btn-warning mb-2 " > <i> <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M5.536 21.886a1.004 1.004 0 0 0 1.033-.064l13-9a1 1 0 0 0 0-1.644l-13-9A1 1 0 0 0 5 3v18a1 1 0 0 0 .536.886z"></path></svg> </i> </button>
 														</form>
+														
 													</div>
 												</div>
 											</div>
@@ -283,7 +288,9 @@
 														</button></a>
 													</div>
 													<div class="">
-														<button  type="submit" name="moveB" class="btn btn-warning mb-2 me-1" > <i> <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="m4.431 12.822 13 9A1 1 0 0 0 19 21V3a1 1 0 0 0-1.569-.823l-13 9a1.003 1.003 0 0 0 0 1.645z"></path></svg> </i> </button>	
+														<form method="post" action="checkGET.php?moveB=' . $row['id'] . '">
+															<button  type="submit" name="moveB" class="btn btn-warning mb-2 me-1" > <i> <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="m4.431 12.822 13 9A1 1 0 0 0 19 21V3a1 1 0 0 0-1.569-.823l-13 9a1.003 1.003 0 0 0 0 1.645z"></path></svg> </i> </button>
+														</form>
 													</div>
 												</div>
 											</div>
